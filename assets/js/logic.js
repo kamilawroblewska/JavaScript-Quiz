ea6c855f6913aec86775a17c8a01e2e8ffbdfdd3
kamilawroblewska <kamila.wroblewska15@gmail.com>
2024-01-14 16:07:29 +0000

    Added DOM elements to logic.js file

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,4 +1,17 @@
-// DOM elements
+// DOM element
+
+const mainCointainer = document.getElementById("start-screen");
+const startButton = document.getElementById("start");
+const quizContainer = document.getElementById("questions");
+const questionElement = document.getElementById("question-title");
+const choicesElement = document.getElementById("choices");
+const endContainer = document.getElementById("end-screen");
+const finalScoreElement = document.getElementById("final-score");
+const initialInput = document.getElementById("initials");
+const submitBtn = document.getElementById("submit");
+const feedbackElement = document.getElementById("feedback");
+const timerEl = document.getElementById("time");
+
 // Event listeners
 // Function starting quiz
 // Function showing questions
