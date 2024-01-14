28b920b49540ead16b05ee1be0dec34e54485ed5
kamilawroblewska <kamila.wroblewska15@gmail.com>
2024-01-14 16:35:54 +0000

    Added list-style-type to none in css file to prevent showing double number list in highscores page

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,2 +1,25 @@
 // Event listener for displaying high scores
+document.addEventListener("DOMContentLoaded", function () {
+  displayHighScores();
+
+  document.getElementById("clear").addEventListener("click", clearHighScores);
+});
+
 // Function displaying list of scores
+function displayHighScores() {
+  const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+  const highScoresList = document.getElementById("highscores");
+
+  highScoresList.innerHTML = "";
+
+  highScores.forEach((score, index) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = `${index + 1}. ${score.initials} - ${score.score}`;
+    highScoresList.appendChild(listItem);
+  });
+}
+
+function clearHighScores() {
+  localStorage.removeItem("highScores");
+  displayHighScores();
+}
